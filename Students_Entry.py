1041f5ab8fa3abaa596d2ae03a4f1c5290336ce8
SevenB1 <qambarmadad12@gmail.com>
2023-10-20 08:46:46 +0500

    Removed the previous code.

--- Students_Entry.py
+++ Students_Entry.py
@@ -1,14 +1,3 @@
-# Students_List = []
-# print("========== STUDENT ENTRY SYSTEM ==========")
-# n = int(input("Number of Students: "))
-# for i in range(n):
-#     print("Student Id      Student Name         Subject 1     Subject 2     Subject 3")
-#     student_info = input("Enter the information asked above separated by commas: ")
-#     info = student_info.split()
-#     students_list = info
-#     Students_List.append(students_list)
-# for i, students_list in enumerate(Students_List):
-#     print(students_list)
 student_records = []
 print("========== STUDENT ENTRY SYSTEM ==========")
 while True:
