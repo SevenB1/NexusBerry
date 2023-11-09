1961dddbab3b4d0a7038883bdbc9411b3a250af9
SevenB1 <qambarmadad12@gmail.com>
2023-11-09 18:07:07 +0500

    Updated code.

--- Courses.py
+++ Courses.py
@@ -11,7 +11,12 @@ class Student:
     def __init__(self,reg_no,name):
         self.reg_no = reg_no
         self.name = name
-        self.courses = []
+        self.admitted_courses = []
+    def admitted_courses(self):
+        if self.admitted_courses:
+            print(f"{self.name} is admitted to following courses")
+            for course in self.admitted_courses:
+                print(f"{Courses.course_title} with credit hours {Courses.credit_hours}")
 
 
 python = Courses(2819,"Python Programming",400)
