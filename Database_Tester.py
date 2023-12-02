ec545ac6ca36a4eca495fdb9afc64b66846982d8
SevenB1 <qambarmadad12@gmail.com>
2023-12-02 22:34:14 +0500

    Database tester

---
+++ Database_Tester.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+# fifa = pd.read_csv('fifa.csv')
+# print(fifa.head(10))
+
+titan = pd.read_csv('titanic_train.csv')
+
+# titan2 = titan.groupby(['Pclass','Sex'])['Sex'].count()
+print(titan.head)
+
